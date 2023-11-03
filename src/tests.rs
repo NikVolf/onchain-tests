ca9b76d27b9cf0eb33b90262f90a0edbc2dfb445
nikvolf <nikvolf@gmail.com>
2023-11-03 16:37:00 +0300

    add failing test

--- src/tests.rs
+++ src/tests.rs
@@ -2,7 +2,7 @@ extern crate gtest;
 extern crate std;
 
 use self::gtest::{Log, Program, System};
-use crate::io::Error;
+use crate::io::{Error, FailType, FailedFixture};
 use crate::service::{Expectation, ExpectedMessage, Fixture, Message, StringIndex};
 use gstd::prelude::*;
 
@@ -121,7 +121,6 @@ fn service_run_empty() {
         .payload_bytes(Result::<(), Error>::Ok(()).encode());
 
     assert!(res.contains(&log));
-
 }
 
 #[test]
@@ -130,10 +129,7 @@ fn res_programs_work() {
     system.init_logger();
 
     let program = Program::from_file(&system, std::path::Path::new("./res/wasm/demo_ping.wasm"));
-    let _res = program.send(
-        OWNER_1,
-        0,
-    );
+    let _res = program.send(OWNER_1, 0);
 
     let res = program.send_bytes(SENDER, b"PING".to_vec());
 
@@ -152,11 +148,9 @@ fn service_run_more() {
 
     let program = Program::current(&system);
 
-    let program_ping = Program::from_file(&system, std::path::Path::new("./res/wasm/demo_ping.wasm"));
-    let _res = program_ping.send(
-        OWNER_1,
-        0,
-    );
+    let program_ping =
+        Program::from_file(&system, std::path::Path::new("./res/wasm/demo_ping.wasm"));
+    let _res = program_ping.send(OWNER_1, 0);
 
     let _res = program.send(
         OWNER_1,
@@ -196,6 +190,65 @@ fn service_run_more() {
         .source(program.id())
         .dest(SENDER)
         .payload_bytes(Result::<(), Error>::Ok(()).encode());
+
+    assert!(res.contains(&log));
+}
+
+#[test]
+fn service_run_failing() {
+    let system = System::new();
+    system.init_logger();
+
+    let program = Program::current(&system);
+
+    let program_ping =
+        Program::from_file(&system, std::path::Path::new("./res/wasm/demo_ping.wasm"));
+    let _res = program_ping.send(OWNER_1, 0);
+
+    let _res = program.send(
+        OWNER_1,
+        io::Init {
+            owner: OWNER_1.into(),
+            service_address: program_ping.id().into_bytes().into(),
+        },
+    );
+
+    let fixture = Fixture {
+        description: StringIndex,
+        preparation: vec![],
+        expectations: vec![Expectation {
+            request: Message {
+                gas: 1_000_000_000,
+                value: 0,
+                payload: b"PING".to_vec(),
+            },
+            response: ExpectedMessage {
+                at_least_gas: None,
+                value: Some(0),
+                payload: Some(b"WRONG REPLY EXPECTED".to_vec()),
+            },
+            fail_hint: StringIndex,
+        }],
+    };
+    let _res = program.send(
+        SENDER,
+        io::Control::AddFixture {
+            fixture: fixture.clone(),
+        },
+    );
+
+    let res = program.send(SENDER, io::Control::RunFixtures);
+
+    let failing_result = vec![FailedFixture {
+        index: 0,
+        fail_type: FailType::PayloadMismatch,
+        fail_hint: Some(StringIndex),
+    }];
+
+    let log = Log::builder()
+        .source(program.id())
+        .dest(SENDER)
+        .payload_bytes(Result::<(), Error>::Err(Error::SomeFailed(failing_result.into())).encode());
 
     assert!(res.contains(&log));
 }
